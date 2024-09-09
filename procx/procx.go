3942a176da87a503be916a1da3151f2087a46f3b
zhiheng123 <903292776@qq.com>
2024-09-09 16:11:25 +0800

    feat: add procx util (#8)
    Signed-off-by: zhiheng123 <903292776@qq.com>

---
+++ procx/procx.go
@@ -0,0 +1,38 @@
+package procx
+
+import (
+	"bytes"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var pidPattern = regexp.MustCompile("[0-9]+")
+
+func ProcessNameContains(processName string) (bool, error) {
+	result := false
+	fileInfos, err := os.ReadDir("/proc")
+	if err != nil {
+		return false, err
+	}
+	for _, info := range fileInfos {
+		name := info.Name()
+		if !pidPattern.MatchString(name) {
+			continue
+		}
+		content, err := os.ReadFile("/proc/" + info.Name() + "/cmdline")
+		if err != nil {
+			// the proc may end during the loop, ignore it
+			continue
+		}
+		if len(content) == 0 {
+			continue
+		}
+		split := strings.Split(string(bytes.TrimRight(content, string("\x00"))), string(byte(0)))
+		cmdLine := strings.Join(split, " ")
+		if strings.Contains(cmdLine, processName) {
+			result = true
+		}
+	}
+	return result, err
+}
