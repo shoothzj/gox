95f395cc42246e3efc6270dd03e8d5299af33764
ZhangJian He <shoothzj@gmail.com>
2024-10-03 13:24:51 +0800

    feat: support flyway migrate sqlite (#25)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- flyway/flyway.go
+++ flyway/flyway.go
@@ -21,7 +21,10 @@ func NewFlyway(dbx *db.Dbx) (*Flyway, error) {
 	flyway := Flyway{
 		dbx: dbx,
 	}
-	exec, err := flyway.Db().Exec(`CREATE TABLE IF NOT EXISTS flyway_schema_history (
+	var err error
+	var exec sql.Result
+	if flyway.dbx.Driver() == db.DriverMySQL {
+		exec, err = flyway.Db().Exec(`CREATE TABLE IF NOT EXISTS flyway_schema_history (
 		installed_rank INT NOT NULL,
 		version VARCHAR(50) COLLATE utf8mb4_bin DEFAULT NULL,
 		description VARCHAR(200) COLLATE utf8mb4_bin NOT NULL,
@@ -35,6 +38,24 @@ func NewFlyway(dbx *db.Dbx) (*Flyway, error) {
 		PRIMARY KEY (installed_rank),
 		KEY flyway_schema_history_s_idx (success)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;`)
+	} else if flyway.dbx.Driver() == db.DriverSqlite {
+		exec, err = flyway.Db().Exec(`CREATE TABLE IF NOT EXISTS flyway_schema_history (
+		installed_rank INTEGER NOT NULL,
+		version TEXT DEFAULT NULL,
+		description TEXT NOT NULL,
+		type TEXT NOT NULL,
+		script TEXT NOT NULL,
+		checksum INTEGER DEFAULT NULL,
+		installed_by TEXT NOT NULL,
+		installed_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		execution_time INTEGER NOT NULL,
+		success INTEGER NOT NULL,
+		PRIMARY KEY (installed_rank),
+		CHECK (success IN (0, 1))
+		);`)
+	} else {
+		return nil, fmt.Errorf("unsupported database driver: %s", flyway.dbx.Driver())
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -101,27 +122,35 @@ func (f *Flyway) Migrate(schemas []Schema) error {
 }
 
 func (f *Flyway) acquireLock() error {
-	var result int
-	err := f.Db().QueryRow("SELECT GET_LOCK('flyway_lock', 10)").Scan(&result)
-	if err != nil {
-		return fmt.Errorf("error acquiring lock: %v", err)
+	if f.dbx.Driver() == db.DriverMySQL {
+		var result int
+		err := f.Db().QueryRow("SELECT GET_LOCK('flyway_lock', 10)").Scan(&result)
+		if err != nil {
+			return fmt.Errorf("error acquiring lock: %v", err)
+		}
+		if result != 1 {
+			return fmt.Errorf("failed to acquire lock, another migration might be running")
+		}
+		return nil
+	} else {
+		return nil
 	}
-	if result != 1 {
-		return fmt.Errorf("failed to acquire lock, another migration might be running")
-	}
-	return nil
 }
 
 func (f *Flyway) releaseLock() error {
-	var result int
-	err := f.Db().QueryRow("SELECT RELEASE_LOCK('flyway_lock')").Scan(&result)
-	if err != nil {
-		return fmt.Errorf("error releasing lock: %v", err)
+	if f.dbx.Driver() == db.DriverMySQL {
+		var result int
+		err := f.Db().QueryRow("SELECT RELEASE_LOCK('flyway_lock')").Scan(&result)
+		if err != nil {
+			return fmt.Errorf("error releasing lock: %v", err)
+		}
+		if result != 1 {
+			return fmt.Errorf("failed to release lock")
+		}
+		return nil
+	} else {
+		return nil
 	}
-	if result != 1 {
-		return fmt.Errorf("failed to release lock")
-	}
-	return nil
 }
 
 // calculateChecksum calculates the CRC32 checksum of the migration script
--- flyway/flyway_test.go
+++ flyway/flyway_test.go
@@ -4,16 +4,17 @@ import (
 	"context"
 	"fmt"
 	"github.com/shoothzj/gox/db"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/stretchr/testify/assert"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/testcontainers/testcontainers-go/modules/mysql"
 )
 
-func TestFlywayWithLocking(t *testing.T) {
+func TestFlywayMysqlMigrate(t *testing.T) {
 	ctx := context.Background()
 
 	container, err := mysql.RunContainer(ctx, mysql.WithDatabase("testdb"))
@@ -48,6 +49,44 @@ func TestFlywayWithLocking(t *testing.T) {
 			Description: "Add email column",
 			Script:      "V2__Add_email.sql",
 			Sql:         `ALTER TABLE users ADD COLUMN email VARCHAR(100);`,
+		},
+	}
+
+	err = flywayInstance.Migrate(schemas)
+	assert.NoError(t, err)
+
+	var migrationCount int
+	err = dbx.Db().QueryRow("SELECT COUNT(*) FROM flyway_schema_history").Scan(&migrationCount)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, migrationCount, "Migrations should be applied")
+}
+
+func TestFlywaySqliteMigrate(t *testing.T) {
+	dsn := ":memory:"
+
+	dbx, err := db.NewDbxFromDsn(db.DriverSqlite, dsn, "")
+	require.NoError(t, err)
+
+	defer dbx.Close()
+
+	err = waitForDB(dbx)
+	require.NoError(t, err)
+
+	flywayInstance, err := NewFlyway(dbx)
+	require.NoError(t, err)
+
+	schemas := []Schema{
+		{
+			Version:     1,
+			Description: "Create users table",
+			Script:      "V1__Create_users.sql",
+			Sql:         `CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);`,
+		},
+		{
+			Version:     2,
+			Description: "Add email column",
+			Script:      "V2__Add_email.sql",
+			Sql:         `ALTER TABLE users ADD COLUMN email TEXT;`,
 		},
 	}
 
