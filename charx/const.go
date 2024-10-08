01e604311460a0fa834e0595d444a853f399e2f3
ZhangJian He <shoothzj@gmail.com>
2024-10-08 23:53:22 +0800

    feat: add greek char define (#28)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- charx/const.go
+++ charx/const.go
@@ -1,5 +1,9 @@
 package charx
 
+const EnglishAlphabetLowerLen = 26
+const EnglishAlphabetUpperLen = 26
+const EnglishAlphabetLen = 52
+
 const EnglishAlphabetLowerStr = "abcdefghijklmnopqrstuvwxyz"
 const EnglishAlphabetUpperStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const EnglishAlphabetStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -7,3 +11,15 @@ const EnglishAlphabetStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 var EnglishAlphabetLowerSlice = []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 var EnglishAlphabetUpperSlice = []rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 var EnglishAlphabetSlice = []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+const GreekAlphabetLowerLen = 24
+const GreekAlphabetUpperLen = 24
+const GreekAlphabetLen = 48
+
+const GreekAlphabetLowerStr = "αβγδεζηθικλμνξοπρστυφχψω"
+const GreekAlphabetUpperStr = "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"
+const GreekAlphabetStr = "αβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"
+
+var GreekAlphabetLowerSlice = []rune{'α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 'χ', 'ψ', 'ω'}
+var GreekAlphabetUpperSlice = []rune{'Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω'}
+var GreekAlphabetSlice = []rune{'α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 'χ', 'ψ', 'ω', 'Α', 'Β', 'Γ', 'Δ', 'Ε', 'Ζ', 'Η', 'Θ', 'Ι', 'Κ', 'Λ', 'Μ', 'Ν', 'Ξ', 'Ο', 'Π', 'Ρ', 'Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω'}
